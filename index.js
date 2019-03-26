bd1ebc2cfacec32d35eb11970a7a98ac308b92c5
MGardner02 <mgardner32@student.umuc.edu>
2019-03-26 08:37:33 -0400

    stripping out extra comments

--- index.js
+++ index.js
@@ -3,7 +3,3 @@
 const file2jsonStream = require('./lib/file2json-stream')
 
 exports.Transform = file2jsonStream
-
-// Order matters use event-stream map function
-
-// Order doesnt matter use event-stream through function
